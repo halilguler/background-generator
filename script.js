ec4cd8e45a9e5ce01a015ea31753b37b0652825c
Halil GÜLER <halilguler1994@gmail.com>
2019-05-24 20:39:34 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const css = document.querySelector("h3");
-const color1 = document.querySelector(".color1");
-const color2 = document.querySelector(".color2");
+const [color1,color2] = document.querySelectorAll("input");
 const body = document.getElementById("gradient");
 
 const setGradient = () => {
-  body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
+  body.style.background = linearGradient();
   css.textContent = body.style.background + ";";
 };
+const linearGradient =()=>{
+	return `linear-gradient(to right,${color1.value},${color2.value})`;
+}
 
-color1.addEventListener("input", setGradient);
+color1.addEventListener('input', setGradient);
 
-color2.addEventListener("input", setGradient);
+color2.addEventListener('input', setGradient);
